e39d9ccd663f615e691aa04e368d3f0758e80a81
kagunecode <cesar.acb98@gmail.com>
2024-09-16 16:30:41 -0500

    Updated tester with data

--- tester.js
+++ tester.js
@@ -1,23 +1,23 @@
 const tasks = require("./tasks");
 
 const resources = {
-	_debug: (stack) => {
-		console.error("DEBUG:", stack);
-	},
+  _debug: (stack) => {
+    console.error("DEBUG:", stack);
+  },
 };
 
 const input = {
-	files: ["file1", "file2", "file3", "file4", "file5"],
-	scanned_files: ["file1", "file4"],
-	errored_files: ["file3"],
+  files: ["file1", "file2", "file3", "file4", "file5"],
+  scanned_files: ["file1", "file4"],
+  errored_files: ["file3"],
 };
 
 (async () => {
-	try {
-		const modInstance = tasks(resources);
-		const result = await modInstance.run(input);
-		console.log("Result:", result);
-	} catch (error) {
-		console.error("Error:", error.message);
-	}
+  try {
+    const modInstance = tasks(resources);
+    const result = await modInstance.run(input);
+    console.log("Result:", result);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 })();
